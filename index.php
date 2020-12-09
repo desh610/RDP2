e5aa9cf0552ace310ca3742ab10d5b90ec75962f
desh610 <deshan.biss@gmail.com>
2020-12-09 21:26:06 +0530

    this is the starting template

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!-- adding boostrap css link -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+<!-- this link will help to import icons -->
+<script src="https://kit.fontawesome.com/2f4a707aac.js" crossorigin="anonymous"></script>
+
+<!-- adding style.css sheet -->
+<link rel="stylesheet" href="style.css">
+
+    <title>Document</title>
+</head>
+<body>
+    <!-- meeee line ekai athara-->
+
+
+    <!-- meeee line ekai athara -->
+
+<!-- adding boostrap js link -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+</body>
+</html>
